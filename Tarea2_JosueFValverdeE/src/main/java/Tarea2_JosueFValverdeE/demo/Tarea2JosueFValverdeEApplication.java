eff84f107de5c650e14ae38b13e440eac14ca200
Josue Valverde <jvalverde40358@ufide.ac.cr>
2023-10-06 02:14:17 -0600

    Tarea#2_JosueValverde

---
+++ Tarea2_JosueFValverdeE/src/main/java/Tarea2_JosueFValverdeE/demo/Tarea2JosueFValverdeEApplication.java
@@ -0,0 +1,13 @@
+package Tarea2_JosueFValverdeE.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea2JosueFValverdeEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea2JosueFValverdeEApplication.class, args);
+	}
+
+}
---
+++ Tarea2_JosueFValverdeE/src/test/java/Tarea2_JosueFValverdeE/demo/Tarea2JosueFValverdeEApplicationTests.java
@@ -0,0 +1,13 @@
+package Tarea2_JosueFValverdeE.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea2JosueFValverdeEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
